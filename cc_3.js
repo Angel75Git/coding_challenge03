90d0faa25f9d5a27b656736aeb14858fafe1ebf8
Angel75Git <aduboncenteno@usf.edu>
2025-02-02 08:19:19 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -1,7 +1,18 @@
 //Task 1 
-let prices = [34, 65, 82, 17, 92]; //Created array
+let prices = [34.99, 65.00, 82.99, 17.99, 92.00]; //Created array
 console.log(prices);
-prices.push(12);           //Changes to array +12
+prices.push(12.99);           //Changes to array +12
 console.log(prices);
 prices.shift();          // Delete 1st index
 console.log(prices);         
+
+//Task 2
+let orders = [13, 12, 25, 21, 18]; //inital customer orders 
+console.log(orders);
+orders[2] += 5;               //add 5 to third order
+console.log(orders);
+
+//Combined all orders by summation
+let total = orders.reduce((sum, order) => sum + order, 0);     
+console.log("Total orders:",total)          
+
